ae3a2982e2f1a9eb7fdf51a5fb65718c908c0203
mohammadhosein <mohammad.h.safari79@gmail.com>
2021-02-18 23:19:18 +0330

    feat: first

---
+++ execute.py
@@ -0,0 +1,28 @@
+# Temp.py
+
+from ds18b20 import DS18B20
+import requests
+import time
+import os
+from datetime import datetime
+
+# test temperature sensors
+x = DS18B20()
+count = x.device_count()
+i = 0
+
+while (True):
+    time.sleep(5)
+    while i < count:
+        uuid = x.read_name(i)
+        temp = x.tempC(i)
+
+        requests.post(os.environ.get('DJANGO_URL'),
+                      data={
+                          'logged_at': datetime.now(),
+                          'temp': temp,
+                          'uuid': uuid
+                      })
+
+        print("ID:  {id} --> temperature:  {temp}".format(id=uuid, temp=temp))
+        i += 1
